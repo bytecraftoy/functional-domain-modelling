7e5c7bc9f077f5516d03bf5ef4f26245cf7da23e
Juha Heljoranta <juha.heljoranta@iki.fi>
2020-11-07 00:54:48 +0200

    f sharp

---
+++ f_sharp/Program.fs
@@ -0,0 +1,45 @@
+﻿type UnvalidatedUserId = UnvalidatedUserId of int
+type UnvalidatedBookId = UnvalidatedBookId of int
+type ValidUserId = ValidUserId of int
+type ValidBookId = ValidBookId of int
+
+type Borrowed = {
+    ValidUserId: ValidUserId
+    ValidBookId: ValidBookId
+}
+
+type BookNotCurrentlyAvailable = {
+    ValidUserId: ValidUserId
+    ValidBookId: ValidBookId
+}
+
+type BookNotFound = {
+    BookId: UnvalidatedBookId
+}
+
+type UserNotFound = {
+    BserId: UnvalidatedUserId
+}
+
+type BorrowFailure =
+  | BookNotCurrentlyAvailable
+  | BookNotFound
+  | UserNotFound
+
+type ValidateUserId = UnvalidatedUserId -> ValidUserId option
+type ValidateBookId = UnvalidatedBookId -> ValidBookId option
+type MarkBookBorrowed = ValidUserId -> ValidBookId -> Borrowed option
+type BorrowBook = 
+    ValidateUserId
+        -> ValidateBookId 
+        -> MarkBookBorrowed 
+        -> UnvalidatedUserId 
+        -> UnvalidatedBookId 
+        -> Result<Borrowed, BorrowFailure>
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
